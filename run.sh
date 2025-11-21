33ac92be426f9c1e21b67786cca3f1a1cc6fe603
Nick Mow <nickmow@gmail.com>
2025-11-21 07:55:17 -0800

    Initial commit

---
+++ run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source .env
+
+if [[ -z "${TENANCY_ID}" ]]; then
+    echo "TENANCY_ID is unset or empty. Please change in .env file"
+    exit 1
+else
+    echo "TENANCY_ID is set correctly"
+fi
+
+# To verify that the authentication with Oracle cloud works
+echo "Checking Connection with this request: "
+oci iam compartment list
+if [ $? -ne 0 ]; then
+    echo "Connection to Oracle cloud is not working. Check your setup and config again!"
+    exit 1
+fi
+
+# ----------------------CUSTOMIZE---------------------------------------------------------------------------------------
+
+# Don't go too low or you run into 429 TooManyRequests
+requestInterval=60 # seconds
+
+# VM params
+cpus=1 # max 4 cores
+ram=2 # max 24gb memory
+bootVolume=50 # disk size in gb
+displayName="WG Relay Instance" # friendly name for instance
+
+profile="DEFAULT"
+
+# ----------------------ENDLESS LOOP TO REQUEST AN ARM INSTANCE---------------------------------------------------------
+
+while true; do
+
+    response=$(oci compute instance launch --no-retry  \
+                --auth api_key \
+                --profile "$profile" \
+                --display-name "$displayName" \
+                --compartment-id "$TENANCY_ID" \
+                --image-id "$IMAGE_ID" \
+                --subnet-id "$SUBNET_ID" \
+                --availability-domain "$AVAILABILITY_DOMAIN" \
+                --shape 'VM.Standard.A1.Flex' \
+                --shape-config "{'ocpus':$cpus,'memoryInGBs':$ram}" \
+                --boot-volume-size-in-gbs "$bootVolume" \
+                --ssh-authorized-keys-file "$PATH_TO_PUBLIC_SSH_KEY" \
+                --raw-output 2>&1)
+
+    sleep $requestInterval
+done
